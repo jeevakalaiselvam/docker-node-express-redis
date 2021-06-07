20d1c7adec8744e5459ffd8b3dfdea86a2abcc2b
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-07 17:10:27 +0530

    visits working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
+
 const app = express();
 const client = redis.createClient({
     host: 'redis-server',
@@ -13,6 +15,9 @@ app.get('/', (req, res) => {
         res.send('No of visits: ' + visits);
         client.set('visits', parseInt(visits) + 1);
     });
+
+    //process.exit(0); Setting restart value in Docker compose to 'always' will restart the container even if exit code is 0
+    //process.exit(1); Setting restart value in Docker compose to 'only-failure' will restart the container only if exit code is other than 0
 });
 
 app.listen(8080, () => {
